84ad87d794174f4114a6447cb0de86dd0884433c
Prathamesh05-source <130919489+Prathamesh05-source@users.noreply.github.com>
2024-08-01 18:44:59 +0530

    Update settings.py

--- user_dashboard/settings.py
+++ user_dashboard/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#l8t6etbn)l&v@8^9^ub-n*o!(md3c4n*(%2f^m9(n33z!h-nj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
