fdb658e7d6e3534a852244e955734f50a153d127
Prathamesh05-source <130919489+Prathamesh05-source@users.noreply.github.com>
2024-08-01 17:56:33 +0530

    Update settings.py

--- user_dashboard/settings.py
+++ user_dashboard/settings.py
@@ -41,9 +41,6 @@ INSTALLED_APPS = [
     'accounts',
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
 LOGIN_REDIRECT_URL = 'dashboard'
 
 MIDDLEWARE = [
@@ -129,7 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
